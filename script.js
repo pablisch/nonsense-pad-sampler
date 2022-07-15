724308ec923d78a505428293a745b9992d39e30d
Pablo <pablito@me.com>
2022-07-15 19:56:43 +0100

    added media

--- script.js
+++ script.js
@@ -30,20 +30,41 @@ for (let i = 0; i < numberOfBeatPanels; i++) {
   })
 }
 
+// Sense keyboard strokes
+
+document.addEventListener("keydown", function(keyWasPressed) {
+  var itWasThisKey = keyWasPressed.key;
+  playSound(itWasThisKey);
+  panelFlash(itWasThisKey);
+})
+
+// Play sound according to keydown OR panel click
+
 function playSound(inputSelector) {
   switch (inputSelector) {
-    case "e":
-      var soundSelected = "boop-sound-effect";
-      break;
-    case "d":
-      var soundSelected = "Sci-fi-beep-sound-effect";
-      break;
+    case "e": var soundSelected = "boop"; break;
+    case "d": var soundSelected = "sciFiBeep"; break;
+    case "x": var soundSelected = "placeholderSound"; break;
+    case "r": var soundSelected = "tom-1"; break;
+    case "f": var soundSelected = "tom-2"; break;
+    case "c": var soundSelected = "tom-4"; break;
+    case "t": var soundSelected = "snare"; break;
+    case "g": var soundSelected = "cymbalCrash"; break;
+    case "v": var soundSelected = "tom-3"; break;
+    case "y": var soundSelected = "sciFiBeep"; break;
+    case "h": var soundSelected = "boop"; break;
+    case "b": var soundSelected = "sciFiBeep"; break;
+    case "u": var soundSelected = "boop"; break;
+    case "j": var soundSelected = "sciFiBeep"; break;
+    case "n": var soundSelected = "sciFiBeep"; break;
     default:
       break;
   }
   var soundToPlay = new Audio("samples/" + soundSelected + ".mp3");
   soundToPlay.play();
 }
+
+// Flash panel when activated
 
 function panelFlash(inputSelector) {
   document.querySelector("." + inputSelector).classList.add("soundActive");
@@ -52,5 +73,6 @@ function panelFlash(inputSelector) {
   }
   setTimeout(panelFashOff, 150);
 }
+
 
 
